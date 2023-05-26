4f7243448f8e66ff65b470feba12ba240bb8cea6
shashipanta <shashipanta57@gmail.com>
2023-05-26 13:58:20 +0545

    Update: Updated duplicate phone number to satisfiy unique constraint

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,10 @@
 INSERT INTO tenant (name, address, email, phone_number, joined_at)
 values ('Shashi kumar Panta', 'Nayabasti, Kathmandu', 'shashipanta57@gmail.com', '9812349875', current_timestamp),
-       ('Samir Bhandari', 'Budanilkantha, Kathmandu', 'bhandarisamir@gmail.com', '9812367489', current_timestamp),
+       ('Samir Bhandari', 'Budanilkantha, Kathmandu', 'bhandarisamir@gmail.com', '9875643562', current_timestamp),
        ('Abhi Kafle', 'Naxal, Kathmandu', 'kafle1Abhi@gmail.com', '9801239874', current_timestamp),
        ('Ram Roka', 'Balaju, Kathmandu', 'ram.roka@gmail.com', '9836782678', current_timestamp),
        ('Hari Roka', 'Jorpati, Kathmandu', 'hari.roka@gmail.com', '9812367489', current_timestamp),
        ('Sita Acharya', 'Balaju, Kathmandu', 'sitaacharya@gmail.com', '9816473562', current_timestamp),
        ('Menuka Thapa', 'Balaju, Kathmandu', 'menuka.123@gmail.com', '9801942789', current_timestamp),
        ('Parbati Kafle', 'Balaju, Kathmandu', 'parbati.kafle33@gmail.com', '9836745612', current_timestamp)
+    ;
