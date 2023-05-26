b751d090810ee32ad7095eb4991406cc83871da2
shashipanta <shashipanta57@gmail.com>
2023-05-26 13:33:26 +0545

    Added data.sql file to load database with initial values
    In order to load we specify this in application.properties
    spring.jpa.defer-datasource-initialization=true
    spring.sql.init.mode=always

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO tenant (name, address, email, phone_number, joined_at)
+values ('Shashi kumar Panta', 'Nayabasti, Kathmandu', 'shashipanta57@gmail.com', '9812349875', current_timestamp ),
+('Samir Bhandari', 'Budanilkantha, Kathmandu', 'bhandarisamir@gmail.com', '9812367489', current_timestamp),
+('Abhi Kafle', 'Naxal, Kathmandu', 'kafle1Abhi@gmail.com', '9801239874', current_timestamp),
+('Ram Roka', 'Balaju, Kathmandu', 'ram.roka@gmail.com', '9836782678', current_timestamp);
